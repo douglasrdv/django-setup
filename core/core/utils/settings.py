a969b3e2e81bede14fa48993a732df1fc0048d76
Douglas <douglasrdv@proton.me>
2024-05-27 22:10:35 -0300

    feat: Add function to return dict from environment prefix

---
+++ core/core/utils/settings.py
@@ -0,0 +1,20 @@
+import os
+
+from .misc import yaml_coerce
+
+
+def get_settings_from_environment(prefix):
+    '''
+    if
+    'CORESETTINGS_IN_DOCKER = 1'
+
+    function returns
+    {
+        'IN_DOCKER': 1
+    }
+
+    '''
+    prefix_len = len(prefix)
+    return {key[prefix_len:]: yaml_coerce(value) for key, value in os.environ.items() if key.startswith(prefix)}
+
+
