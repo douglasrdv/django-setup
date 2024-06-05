c872ad5f1e71d8bf8b7352dda530dd498fc01d08
Douglas <douglasrdv@proton.me>
2024-06-04 21:51:47 -0300

    WIP

--- core/core/utils/settings.py
+++ core/core/utils/settings.py
@@ -16,5 +16,3 @@ def get_settings_from_environment(prefix):
     '''
     prefix_len = len(prefix)
     return {key[prefix_len:]: yaml_coerce(value) for key, value in os.environ.items() if key.startswith(prefix)}
-
-
---
+++ core/project/settings/envars.py
@@ -0,0 +1,5 @@
+from core.core.utils.collections import deep_update
+from core.core.utils.settings import get_settings_from_environment
+
+
+
